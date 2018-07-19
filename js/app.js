5225ec31881489a1709df1278499449e99aebcd8
Simon-Whitaker <simon.whitaker9@gmail.com>
2018-07-19 22:09:31 +0100

    Added pseudo code

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
+    // x pos
+    // y pos
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +17,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // if enemny has not passed edge of game canvas
+      // move forward
+      // incriment x by speed * dt
+    // else
+      // reset pos to start
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,14 +31,40 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// PLAYER SPRITE
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+    // constructor
+
+    // properties
+      // x pos
+      // y pos
+      // sprite images
+
+    // methods
+      // update postion
+        // check check collision
+          // did sprite x & y collide with enemy?
+        // reach the end of game
+            // did sprite x & y reach final tile
+      // render
+        // draw player sprite on current x & y coord pos
+      //handle keyboard input
+        //update sprites x & y according to input
+      // reset player sprite
+        // set x & y to starting x & y
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+    // init allEnemies array
+    // for each enemy create and push new enemy object into above array 
 
 
 
