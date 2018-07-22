4f311b1b29aa616b6112ab571294f68441c22a59
Simon-Whitaker <simon.whitaker9@gmail.com>
2018-07-22 16:02:25 +0100

    Added / Amended code comments

--- js/app.js
+++ js/app.js
@@ -1,29 +1,25 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    this.sprite = 'images/enemy-bug.png';   // sprite image
+    this.sprite = 'images/enemy-bug.png';   // enemy sprite image
     this.x = x;   // x pos
     this.y = y + 40;   // y pos
     this.horizontal = 101;    // distance moved each step right
     this.speed = speed;
     this.startPos = -101;   // enemy start position
     this.endPos = this.horizontal * 5;    // enemy end position
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Enemy Movement
+  /*
+  Parameter: dt, a time delta between ticks
+  Movement is multiplied by dt para to ensure the game runs at the same speed for all computers
+  */
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
     if(this.x < this.endPos){   // if enemny has not passed edge of game canvas
       this.x += this.speed * dt;   // move forward - incriment x by speed * dt
     }
     else {
-      this.x = this.startPos;   // reset pos to start
+      this.x = this.startPos;   // reset enemy pos to start
     }
 
 };
@@ -33,11 +29,6 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// PLAYER SPRITE
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 // Player character constructor
 class Character {
@@ -54,71 +45,61 @@ class Character {
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
-  //update sprites x & y according to input
+  //update sprites x & y according to keyboard input
   handleInput(input) {
     switch(input){
       case 'left':
-        if (this.x > 0) {
-           this.x -= this.horizontal;
+        if (this.x > 0) {   // if character isn't at left edge
+           this.x -= this.horizontal;   // move character to the left
         }
         break;
       case 'right':
-        if (this.x < this.horizontal * 4) {
-           this.x += this.horizontal;
+        if (this.x < this.horizontal * 4) {   // if character isn't at right edge
+           this.x += this.horizontal;   // move character to the right
         }
         break;
       case 'up':
-        if (this.y > 0) {
-          this.y -= this.vertical;
+        if (this.y > 0) {   // if character isn't at top edge
+          this.y -= this.vertical;    // move character up
         }
         break;
       case 'down':
-      if (this.y < this.vertical * 4) {
-        this.y += this.vertical;
+      if (this.y < this.vertical * 4) {   // if character isn't at edge
+        this.y += this.vertical;    // move character down
       }
         break;
     }
   }
-    // methods
-      // update postion
+      // check for collision or end game
       update() {
         console.log(this.y);
-        // check check collision
-        for(let enemy of allEnemies) {
-          if (this.y === enemy.y && (enemy.x + enemy.horizontal/2 > this.x && enemy.x < this.x +this.horizontal/2)){
-            this.restart();
+        for(let enemy of allEnemies) {    // enemy array
+          if (this.y === enemy.y && (enemy.x + enemy.horizontal/2 > this.x && enemy.x < this.x +this.horizontal/2)){    // enemy & character in same postion
+            this.restart();   // restart character
           }
         }
-          // did sprite x & y collide with enemy?
-        if(this.y === -43){
-          alert('You Won!');
-          this.restart();
-        }// reach the end of game
-            // did sprite x & y reach final tile
+        if(this.y === -43){   // check if character reached the top / end game
+          alert('You Won!');    // win alert
+          this.restart();   // restart character
+        }
       }
-      //handle keyboard input
-        //update sprites x & y according to input
+      // restart character code
       restart() {
         this.x = this.startX;
         this.y = this.startY;
-      }// reset player sprite
-        // set x & y to starting x & y
+      }
 }
 
 
-// Now instantiate your objects.
+// Iinitiate objects.
 const player = new Character();   // Player character object
-const enemy1 = new Enemy(-101, 0, 150);   // Enemy character object
-const enemy2 = new Enemy(-101, 83, 200);   // Enemy character object
-const enemy3 = new Enemy((-101*1.5), 166, 300);   // Enemy character object
-// Place all enemy objects in an array called allEnemies
+const enemy1 = new Enemy(-101, 0, 150);   // Enemy character object 1
+const enemy2 = new Enemy(-101, 83, 200);   // Enemy character object 2
+const enemy3 = new Enemy((-101*1.5), 166, 300);   // Enemy character object 3
+
+// Enemy object array
 const allEnemies = [];
-allEnemies.push(enemy1, enemy2, enemy3);
-
-    // init allEnemies array
-    // for each enemy create and push new enemy object into above array
-
-
+allEnemies.push(enemy1, enemy2, enemy3);    // push new enemy object into enemy array
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
